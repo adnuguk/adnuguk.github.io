07c2ba0de966dcf19aa0bb5633638094c9e50c3e
Gary Ewan Park <gep13@gep13.co.uk>
2023-06-12 20:09:21 +0100

    (maint) Switch to using Google Analytics 4
    The existing Google Analytics that we are using is being removed, so we
    have to switch over to use the new one.

--- input/assets/js/disqus.js
+++ input/assets/js/disqus.js
@@ -1,30 +1,7 @@
 /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-var metaElements = document.getElementsByTagName('meta');
-
 var disqus_shortname = 'aberdeendevelopersnetusergroup'; // required: replace example with your forum shortname
-var disqus_identifier;
-var disqus_title;
-var disqus_url;
-
-for (var i = 0; i < metaElements.length; i++) {
-    var element = metaElements[i];
-    var propAttribute = element.getAttribute('property');
-    if (propAttribute === 'og:title') {
-        disqus_title = element.getAttribute('content');
-    }
-    else if (propAttribute === 'og:url') {
-        disqus_url = element.getAttribute('content');
-        disqus_identifier = disqus_url.split(/\//).pop();
-    }
-}
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
-(function() {
-    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-})();
-
 (function () {
     var s = document.createElement('script'); s.async = true;
     s.type = 'text/javascript';
