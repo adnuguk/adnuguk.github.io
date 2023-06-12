97158f20c3019859304947119b8eeb5923a45870
Gary Ewan Park <gep13@gep13.co.uk>
2023-06-12 20:26:47 +0100

    (maint) Fix broken disqus

--- input/assets/js/disqus.js
+++ input/assets/js/disqus.js
@@ -1,10 +1,25 @@
-/* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+var metaElements = document.getElementsByTagName('meta');
+
 var disqus_shortname = 'aberdeendevelopersnetusergroup'; // required: replace example with your forum shortname
+var disqus_identifier;
+var disqus_title;
+var disqus_url;
+
+for (var i = 0; i < metaElements.length; i++) {
+    var element = metaElements[i];
+    var propAttribute = element.getAttribute('property');
+    if (propAttribute === 'og:title') {
+        disqus_title = element.getAttribute('content');
+    }
+    else if (propAttribute === 'og:url') {
+        disqus_url = element.getAttribute('content');
+        disqus_identifier = disqus_url.split(/\//).pop();
+    }
+}
 
 /* * * DON'T EDIT BELOW THIS LINE * * */
-(function () {
-    var s = document.createElement('script'); s.async = true;
-    s.type = 'text/javascript';
-    s.src = '//' + disqus_shortname + '.disqus.com/count.js';
-    (document.getElementsByTagName('HEAD')[0] || document.getElementsByTagName('BODY')[0]).appendChild(s);
-}());
+(function() {
+    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+})();
